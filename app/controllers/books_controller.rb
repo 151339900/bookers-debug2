078d7f2a629f775c204843d419ab7caa3b4aa7c0
151339900 <165742289+151339900@users.noreply.github.com>
2024-05-09 07:24:04 +0000

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
+before_action :authenticate_user!  
 
   def show
     @book = Book.find(params[:id])
-    
   end
 
   def index
@@ -46,4 +46,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body)
   end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+     unless @book.user == current_user
+       redirect_to books_path
+     end    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
-  get "home/about"=>"homes#about"
-
+  get "home/about"=>"homes#about", as: 'about'
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
+    resource :favorite, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
