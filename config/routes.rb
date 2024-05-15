701b336e17595e183ddc5c0c6de72189d1b55843
151339900 <165742289+151339900@users.noreply.github.com>
2024-05-15 08:06:13 +0000

    exercise6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about", as: 'about'
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
 
   end
+  get "search" => "searches#search"
+  
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
   	get "followings" => "relationships#followings", as: "followings"
@@ -15,7 +19,7 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :users
+  
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
